d7ab84c5a48bc503d711ae0461a4d301f0729163
Asemanijoshua <joshuaasemani27@gmail.com>
2024-12-12 11:35:36 +0000

    Created Course Details Page

--- AuthenticateLogin.php
+++ AuthenticateLogin.php
@@ -1,36 +1,8 @@
+
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $recaptchaResponse = $_POST['g-recaptcha-response'];
-    // $secretKey = 'YOUR_SECRET_KEY';
-    $userIp = $_SERVER['REMOTE_ADDR'];
-    $verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=6LcyQWsqAAAAAMuqgUKGQy67lB5W2L2FT2Aqt8L9&response={$recaptchaResponse}&remoteip={$userIp}";
+    include("ConnectDB.php");
 
-    $response = file_get_contents($verificationUrl);
-    $responseData = json_decode($response);
+    
 
-    if ($responseData->success) {
-        echo "<script>
-            Swal.fire({
-                title: 'Success!',
-                text: 'reCAPTCHA verified! You are logged in.',
-                icon: 'success',
-                timer: 2000,
-                showConfirmButton: false
-            }).then(function() {
-                window.location.href = 'courses.html'; // Replace with your login page URL
-            });
-        </script>";
-    } else {
-        echo "<script>
-            Swal.fire({
-                title: 'Error!',
-                text: 'reCAPTCHA verification failed. Please try again.',
-                icon: 'error'
-            }).then(function() {
-                window.history.back();
-            });
-        </script>";
-    }
-}
 ?>
---
+++ AuthenticateSignup.php
@@ -0,0 +1,8 @@
+<?php
+
+    include("ConnectDB.php");
+
+    
+
+
+?>
