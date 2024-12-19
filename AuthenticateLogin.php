e47f6221cdb942df5fc43739b68557c4931620dd
Asemanijoshua <joshuaasemani27@gmail.com>
2024-12-19 12:12:40 +0000

    Switched between files

--- AuthenticateLogin.php
+++ AuthenticateLogin.php
@@ -7,47 +7,6 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
-    
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    $recaptchaResponse = $_POST['g-recaptcha-response'];
-    // $secretKey = 'YOUR_SECRET_KEY';
-    $userIp = $_SERVER['REMOTE_ADDR'];
-    $verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=6LfEFJ4qAAAAAJ8hJ6_CfblpMyrIkFcJd6F1y1vK&response={$recaptchaResponse}&remoteip={$userIp}";
-
-    $response = file_get_contents($verificationUrl);
-    $responseData = json_decode($response);
-
-    if ($responseData->success) {
-        echo "<script>
-            Swal.fire({
-                title: 'Success!',
-                text: 'reCAPTCHA verified! Wait while we login you in.',
-                icon: 'success',
-                timer: 5000,
-                showConfirmButton: false
-            })
-        </script>";
-        // // .then(function() {
-        //     window.location.href = 'AuthenticateLogin.php'; // Replace with your login page URL
-        // });
-    } else {
-        echo "<script>
-            Swal.fire({
-                title: 'Error!',
-                text: 'reCAPTCHA verification failed. Please try again.',
-                icon: 'error'
-                timer: 5000,
-            }).then(function() {
-                window.history.back();
-            });
-        </script>";
-    }
-}
-?>
-
-
 <?php
 
     session_start();
